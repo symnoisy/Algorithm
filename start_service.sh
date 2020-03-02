d03476179b8018889aa3af6b368e042bd351cfa1
symnoisy <symnoisy@gmail.com>
2020-03-02 23:52:41 +0900

    bash script 디렉토리 생성 및 이동

--- start_service.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-nohup python3.6 ./algorithmservicestart.py > algorithmservice.log &
---
+++ util_bash/attach_docker.sh
@@ -0,0 +1,5 @@
+#Docker sh 접속
+#!/usr/bin/env bash
+
+echo "Container login start"
+docker exec -it Algorithm /bin/sh
