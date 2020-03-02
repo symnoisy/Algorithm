b72061edd586a3e137dd07230795cb816e9ea50d
symnoisy <symnoisy@gmail.com>
2020-03-03 00:11:49 +0900

    build/up docker 스크립트 생성

---
+++ util_bash/build_docker.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Docker build start. Target_services=python"
+cd ../docker-compose
+docker-compose build python
---
+++ util_bash/up_docker.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "Docke up start. Target_services=python"
+cd ../docker-compose
+docker-compose up python
+
